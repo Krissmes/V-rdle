525a570d5897fd86773aa3ecb0030738e6a84610
funnycat53 <patriks.jkb@gmail.com>
2024-05-24 17:30:30 +0300

    gatavais produkts

--- main/script.js
+++ main/script.js
@@ -1,46 +1,222 @@
+// importē visus vārdus no faila words.js
 import { WORDS } from "./words.js";
 
-const NUMBER_OF_GUESSES = 6;           // konstants mainīgais, lai noteiktu minējumu skaitu
-let guessesRemaining = NUMBER_OF_GUESSES;       // mainīgais, kas sekos līdzi atlikušajiem minējumiem
-let currentGuess = [];      // tiek saglabāti spēlētāja minētie burti
-let nextLetter = 0;        // mainīgais, kas seko līdzi nākamā minētā burta indeksam
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]      // tiek izvēlēts random vārds no faila VARDI
+// minējumu skaits
+const NUMBER_OF_GUESSES = 6;
+// mainīgais, kas seko līdzi atlikušajiem minējumiem
+let guessesRemaining = NUMBER_OF_GUESSES;
+// saglabā minējumu
+let currentGuess = [];
+// seko līdzi nākamajam rakstītajam burtam
+let nextLetter = 0;
+// mainīgais, lai saglabātu minēto vārdu
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
 
-console.log(rightGuessString)       // konsolē parāda to random vārdu   
+// konsolē parādas minētais vārds
+console.log(rightGuessString);
 
-function initBoard() {        // tiek definēta funkcija initBoard
-    let board = document.getElementById("game-board");        // iegūst HTML elementu "game-board" un izveido to par mainīgo ar nosaukumus board
+// funkcija, kas izveido spēles laukumu HTML formātā
+function initBoard() {
+  let board = document.getElementById("game-board");
 
-    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {       // iterē caur katru no NUMBER_OF_GUESSES, lai izveidotu rindas
-        let row = document.createElement("div")     // izveido jaunu div elementu katrai rindai
-        row.className = "letter-row"        // izveido CSS nosaukumu
-        
-        for (let j = 0; j < 5; j++) {       // iterē katru no 5 lodziņiem pēc kārtas, lai izveidotu burtu lodziņus
-            let box = document.createElement("div")     // izveido jaunu div elementu katram burtu lodziņam
-            box.className = "letter-box"        // izveido CSS nosaukumu
-            row.appendChild(box)        // pievieno rindai burtu lodziņu
-        }
+  // izveido rindas
+  for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+    let row = document.createElement("div");
+    row.className = "letter-row";
 
-        board.appendChild(row)      // pievieno rindu spēles laukumam
+    // izveido kastītes
+    for (let j = 0; j < 5; j++) {
+      let box = document.createElement("div");
+      box.className = "letter-box";
+      row.appendChild(box);
     }
+
+    board.appendChild(row);
+  }
 }
 
+// funkcija minētā burta kastītes krāsas mainīšanai
 function shadeKeyBoard(letter, color) {
-    for (const elem of document.getElementsByClassName("keyboard-button")) {
-      if (elem.textContent === letter) {
-        let oldColor = elem.style.backgroundColor;
-        if (oldColor === "green") {
-          return;
-        }
-  
-        if (oldColor === "yellow" && color !== "green") {
-          return;
-        }
-  
-        elem.style.backgroundColor = color;
-        break;
+  for (const elem of document.getElementsByClassName("keyboard-button")) {
+    if (elem.textContent === letter) {
+      let oldColor = elem.style.backgroundColor;
+      if (oldColor === "green") {
+        return;
+      }
+
+      if (oldColor === "yellow" && color !== "green") {
+        return;
+      }
+
+      elem.style.backgroundColor = color;
+      break;
+    }
+  }
+}
+
+// funkcija, lai dzēstu burtu no pašreizējā minējuma
+function deleteLetter() {
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+  let box = row.children[nextLetter - 1];
+  box.textContent = "";
+  box.classList.remove("filled-box");
+  currentGuess.pop();
+  nextLetter -= 1;
+}
+
+// funkcija, lai pārbaudītu, vai pašreizējais minējums atbilst pareizajam vārdam
+function checkGuess() {
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+  let guessString = "";
+  let rightGuess = Array.from(rightGuessString);
+
+  for (const val of currentGuess) {
+    guessString += val;
+  }
+
+  // pārbauda minējuma garumu un esamību
+  if (guessString.length != 5) {
+    toastr.error("Pārāk īss vārds!");
+    return;
+  }
+
+  if (!WORDS.includes(guessString)) {
+    toastr.error("Vārds nav sarakstā!");
+    return;
+  }
+
+  var letterColor = ["gray", "gray", "gray", "gray", "gray"];
+
+  // pārbaude vai ir pareizs burts
+  for (let i = 0; i < 5; i++) {
+    if (rightGuess[i] == currentGuess[i]) {
+      letterColor[i] = "green";
+      rightGuess[i] = "#";
+    }
+  }
+
+  // pārbaude vai tas ir pareizais burts, bet nepareizajā kastītē
+  for (let i = 0; i < 5; i++) {
+    if (letterColor[i] == "green") continue;
+
+    for (let j = 0; j < 5; j++) {
+      if (rightGuess[j] == currentGuess[i]) {
+        letterColor[i] = "yellow";
+        rightGuess[j] = "#";
       }
     }
   }
+
+  // atjaunina spēles laukumu ar krāsainiem lodziņiem, kas attiecīgi attēlo pareizos vai nepareizos minētos burtus
+  for (let i = 0; i < 5; i++) {
+    let box = row.children[i];
+    let delay = 250 * i;
+    setTimeout(() => {
+      animateCSS(box, "flipInX");
+      box.style.backgroundColor = letterColor[i];
+      shadeKeyBoard(guessString.charAt(i) + "", letterColor[i]);
+    }, delay);
+  }
+
+  // ja minējums ir pareizs, tiek parādīts "Jūs uzminējāt"
+  if (guessString === rightGuessString) {
+    toastr.success("Jūs uzminējāt!");
+    guessesRemaining = 0;
+    return;
+  } else {
+    // samazina attlikušos minējumus 
+    guessesRemaining -= 1;
+    currentGuess = [];
+    nextLetter = 0;
+
+    // parāda "Jums beidzās mēģinājumi!", ja beidzās minējumi, kā arī parāda pareizo vārdu
+    if (guessesRemaining === 0) {
+      toastr.error("Jums beidzās mēģinājumi!");
+      toastr.info(`Pareizais vārds bija: "${rightGuessString}"`);
+    }
+  }
+}
+
+// funkcija, kas ievieto burtu kastītē
+function insertLetter(pressedKey) {
+  if (nextLetter === 5) {
+    return;
+  }
+  pressedKey = pressedKey.toLowerCase();
+
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+  let box = row.children[nextLetter];
+  animateCSS(box, "pulse");
+  box.textContent = pressedKey;
+  box.classList.add("filled-box");
+  currentGuess.push(pressedKey);
+  nextLetter += 1;
+}
+
+// funkcija, kas animē CSS klases
+const animateCSS = (element, animation, prefix = "animate__") =>
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = element;
+    node.style.setProperty("--animate-duration", "0.3s");
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd, { once: true });
+  });
+
+// klausās tastatūras ievadi
+document.addEventListener("keyup", (e) => {
+  if (guessesRemaining === 0) {
+    return;
+  }
+
+  let pressedKey = String(e.key);
+
+   // pārbauda vai uzspiestā poga ir viena no šīm, ja ir, tad burts neparādas
+   if (pressedKey === "q" || pressedKey === "w" || pressedKey === "y" || pressedKey === "x") {
+    return;
+  }
+
+  if (pressedKey === "Backspace" && nextLetter !== 0) {
+    deleteLetter();
+    return;
+  }
+
+  if (pressedKey === "Enter") {
+    checkGuess();
+    return;
+  }
+
+  // pārbauda vai ir derīga ievade no tastatūras
+  let found = pressedKey.match(/[a-zāčēģīķļņšūž]/gi);
+  if (!found || found.length > 1) {
+    return;
+  } else {
+    insertLetter(pressedKey);
+  }
+});
+
+// klausās virtuālas tastatūras ievadi
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+  const target = e.target;
+
+  if (!target.classList.contains("keyboard-button")) {
+    return;
+  }
+  let key = target.textContent;
+
+  if (key === "Del") {
+    key = "Backspace";
+  }
+
+  document.dispatchEvent(new KeyboardEvent("keyup", { key: key }));
+});
 
 initBoard();
--- main/tekstaparveide.py
+++ main/tekstaparveide.py
@@ -1,9 +1,9 @@
-with open("main/words.txt","r", encoding="utf-8") as f:
+with open("main/words.js","r", encoding="utf-8") as f:
     saturs = f.readlines()
     jaunais = [saturs[0]]
     for rinda in saturs[1:]:
         rinda = rinda.strip("\n")
-        rakstamais = "\'" + rinda + "\', "
+        rakstamais = "\'" + rinda + "\', \n"
         jaunais.append(rakstamais)
 
 with open("main/words_teksts.js","w", encoding="utf-8") as f:
