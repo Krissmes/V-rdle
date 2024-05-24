22736ae73b0f915c6b1ca2ddfbf3259ba24d45f4
funnycat53 <patriks.jkb@gmail.com>
2024-05-24 13:11:46 +0300

    new

--- main/script.js
+++ main/script.js
@@ -1,14 +1,15 @@
-import { VARDI } from "./words.js";
+import { WORDS } from "./words.js";
 
 const NUMBER_OF_GUESSES = 6;           // konstants mainīgais, lai noteiktu minējumu skaitu
 let guessesRemaining = NUMBER_OF_GUESSES;       // mainīgais, kas sekos līdzi atlikušajiem minējumiem
 let currentGuess = [];      // tiek saglabāti spēlētāja minētie burti
 let nextLetter = 0;        // mainīgais, kas seko līdzi nākamā minētā burta indeksam
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]      // tiek izvēlēts random vārds no faila VARDI
+
 console.log(rightGuessString)       // konsolē parāda to random vārdu   
 
-function sakumaLauks() {        // tiek definēta funkcija sakumaLaukums
-    let lauks = document.getElementById("speles-lauks");        // iegūst HTML elementu "speles-lauks" un izveido to par mainīgo ar nosaukumus lauks
+function initBoard() {        // tiek definēta funkcija initBoard
+    let board = document.getElementById("game-board");        // iegūst HTML elementu "game-board" un izveido to par mainīgo ar nosaukumus board
 
     for (let i = 0; i < NUMBER_OF_GUESSES; i++) {       // iterē caur katru no NUMBER_OF_GUESSES, lai izveidotu rindas
         let row = document.createElement("div")     // izveido jaunu div elementu katrai rindai
@@ -20,8 +21,26 @@ function sakumaLauks() {        // tiek definēta funkcija sakumaLaukums
             row.appendChild(box)        // pievieno rindai burtu lodziņu
         }
 
-        lauks.appendChild(row)      // pievieno rindu spēles laukumam
+        board.appendChild(row)      // pievieno rindu spēles laukumam
     }
 }
 
-sakumaLauks()       // izsauc funkciju sakumaLauks
+function shadeKeyBoard(letter, color) {
+    for (const elem of document.getElementsByClassName("keyboard-button")) {
+      if (elem.textContent === letter) {
+        let oldColor = elem.style.backgroundColor;
+        if (oldColor === "green") {
+          return;
+        }
+  
+        if (oldColor === "yellow" && color !== "green") {
+          return;
+        }
+  
+        elem.style.backgroundColor = color;
+        break;
+      }
+    }
+  }
+
+initBoard();
